75885ae7c454117098f3fac898d3a176a2b12815
blueberry <kuangyongchang@gmail.com>
2022-02-14 15:01:38 +0800

    Day 1

---
+++ Day_1/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+let count = 100;
+while (count > 0) {
+  console.log(count);
+  count--;
+}
+
+const num = 3.1427;
+console.log(num.toFixed(2));
---
+++ Day_1/test_run.js
@@ -0,0 +1,20 @@
+console.log("Hi GA!");
+
+const total = 0;
+const number = 7;
+const fruit = "apple";
+
+let sum;
+let input = 17;
+let tree = "orange";
+
+//concat String
+
+const currName = "Lily";
+const currRole = "dancer";
+
+const concatStr = "Hello, I'm " + currName + " and I'm a " + currRole;
+console.log(concatStr);
+
+const outputStr = `Hello, I'm ${currName} and I'm a ${currRole}`;
+console.log(concatStr);
